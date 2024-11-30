457ff3ad28b614225e2ea0c57b9303346ed0eb84
Civan Rumet Arı <cari@k1m30s02.42istanbul.com.tr>
2024-12-01 02:41:51 +0300

    Initial commit

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cari <cari@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 02:18:43 by cari              #+#    #+#             */
+/*   Updated: 2024/12/01 02:18:44 by cari             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
