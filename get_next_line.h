8c1f5b799b211a397cf8977dea4377c6470aecd4
Civan Rumet Arı <civanrumet@hotmail.com>
2025-01-15 08:04:55 +0300

    Update

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: urmet <urmet@student.42.fr>                +#+  +:+       +#+        */
+/*   By: cari <cari@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 02:18:43 by cari              #+#    #+#             */
-/*   Updated: 2024/12/21 23:05:16 by urmet            ###   ########.fr       */
+/*   Updated: 2025/01/15 07:51:41 by cari             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,12 @@
 #  define BUFFER_SIZE 1024
 # endif
 
+size_t	ft_strlen(char *s);
 char	*get_next_line(int fd);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_readed_line(char *src);
+char	*ft_strchr(char *s, int c);
+char	*ft_move_start(char *start);
+char	*ft_strjoin(char *s1, char *s2);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,20 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: urmet <urmet@student.42.fr>                +#+  +:+       +#+        */
+/*   By: cari <cari@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 02:41:14 by cari              #+#    #+#             */
-/*   Updated: 2025/01/13 16:11:26 by urmet            ###   ########.fr       */
+/*   Updated: 2025/01/15 08:00:24 by cari             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+size_t	ft_strlen(char	*s)
 {
-	char				*str;
-	long unsigned int	len1;
-	long unsigned int	len2;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	if (c == 0)
+	{
+		i = ft_strlen((char *)s);
+		return (&s[i]);
+	}
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char) c)
+			return (&s[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	size_t	len1;
+	size_t	len2;
 
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
@@ -26,5 +59,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ft_memcpy(str, s1, len1);
 	ft_memcpy(str + len1, s2, len2);
 	str[len1 + len2] = '\0';
+	free((char *)s1);
 	return (str);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	void	*tmp;
+
+	if (!dst && !src)
+		return (NULL);
+	tmp = dst;
+	while (n--)
+		*(char *)dst++ = *(char *)src++;
+	return (tmp);
 }
